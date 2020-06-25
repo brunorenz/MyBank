f5aea0c51c7a86eb3d73cfe5572fbb03a80b6e11
brunorenz <65bruno@gmail.com>
2020-06-25 11:53:25 +0200

    update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,48 @@
-self.addEventListener("fetch", function(event) {
-  // do nothing here, just log all the network requests
-  console.log(event.request.url);
+const CACHE_NAME = "mb-cache-V2";
+const urlsToCache = [
+  "/",
+  "/dist/img/icons/favicon-16x16.png",
+  "/dist/img/icons/favicon-32x32.png",
+  "/dist/img/icons/iconfinder_walletmoneyshoppingatmcard_128.png",
+  "/dist/img/icons/iconfinder_walletmoneyshoppingatmcard_192.png",
+  "/dist/img/icons/iconfinder_walletmoneyshoppingatmcard_256.png",
+  "/dist/img/icons/iconfinder_walletmoneyshoppingatmcard_512.png",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.error("An error occurred: " + err);
+      })
+  );
+  console.log("MyBankSW installed");
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (!CACHE_NAME.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("MyBankSW " + event.request.url);
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
