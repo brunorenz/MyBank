1f52537c710d758ef4087068d7fc563dab432afc
brunorenz <65bruno@gmail.com>
2020-06-26 08:34:57 +0200

    update

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -13,7 +13,7 @@
         <!--
   <span class="navbar-toggler-icon"></span>
   d-sm-none
--->
+        -->
         <span v-b-toggle.sidebar-border class="navbar-toggler-icon"></span>
         <b-sidebar
           id="sidebar-border"
@@ -25,19 +25,14 @@
           <div class="px-3 py-2">
             <nav class="mb-3">
               <b-nav vertical>
-                <b-nav-item active @click="hide">Active</b-nav-item>
-                <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
-                <b-nav-item href="#link-2" @click="hide"
-                  >Another Link</b-nav-item
-                >
+                <b-nav-item active @click="checkLocalStorage">Active</b-nav-item>
+                <b-nav-item href="#link-1" @click="checkLocalStorage">Link</b-nav-item>
+                <b-nav-item href="#link-2" @click="checkLocalStorage">Another Link</b-nav-item>
               </b-nav>
             </nav>
           </div>
         </b-sidebar>
-        <b-navbar-toggle
-          target="navbar-toggle-collapse"
-          class="d-none d-sm-block"
-        >
+        <b-navbar-toggle target="navbar-toggle-collapse" class="d-none d-sm-block">
           <template v-slot:default="{ expanded }">
             <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
             <b-icon v-else icon="chevron-bar-down"></b-icon>
@@ -49,13 +44,9 @@
             <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
             <b-nav-item class="px-3" href="#">Link 1</b-nav-item>
             <b-nav-item class="px-3" href="#">Link 2</b-nav-item>
-            <b-nav-item class="px-3" href="#" disabled
-              >Configurazione</b-nav-item
-            >
+            <b-nav-item class="px-3" href="#" disabled>Configurazione</b-nav-item>
             <b-nav-item-dropdown class="px-3" text="Programmazione">
-              <b-dropdown-item to="/programmazione/termostato"
-                >Termostato</b-dropdown-item
-              >
+              <b-dropdown-item to="/programmazione/termostato">Termostato</b-dropdown-item>
               <b-dropdown-item to="/programmazione/luce">Luce</b-dropdown-item>
             </b-nav-item-dropdown>
           </b-navbar-nav>
@@ -73,7 +64,9 @@
     </div>
     <div class="mr-auto">
       <span class="mr-1">Powered by</span>
-      <a><b>brunorenz@github</b></a>
+      <a>
+        <b>brunorenz@github</b>
+      </a>
     </div>
   </div>
 </template>
@@ -86,9 +79,21 @@ export default {
   name: "MainContainer",
   components: {},
   computed: {
-    name() {
-      return this.$route.name;
-    },
+    // name() {
+    //   return this.$route.name;
+    // }
   },
+  beforeMount: function() {
+    //this.checkLocalStorage();
+  },
+  data : function() {
+    return {};
+  },
+  methods: {
+    checkLocalStorage() {
+      if (window.localStorage) console.log("Local Storage Supported");
+      else console.log("Local Storage Not Supported");
+    }
+  }
 };
 </script>
